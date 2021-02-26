3792f37c0dffd001d292c3b07bd98e2e28e7d1ef
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2021-02-26 11:38:21 +0100

    fixed login fields

--- api/login.js
+++ api/login.js
@@ -1,8 +1,8 @@
 // Vérifie la présence paramètres et fait passer la demande de connexion
 
 exports.run = async(data, path, req, res) => {
-    if (req.body && req.body.login && req.body.password) {
-        data.auth.login(data, req.body.login, req.body.password, req, res);
+    if (req.body && req.body.username && req.body.password) {
+        data.auth.login(data, req.body.username, req.body.password, req, res);
     } else {
         res.json({ error: "Paramètres incomplets" });
     }
